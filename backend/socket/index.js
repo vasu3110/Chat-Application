a3160461bec9127bf98f49a4dbbc5f7f4f113b1a
Vasu Sharma <vasu22300@gmail.com>
2025-06-30 02:05:55 +0530

    First commit

---
+++ backend/socket/index.js
@@ -0,0 +1,24 @@
+const { Server } = require("socket.io");
+const registerHandlers = require("./handlers");
+
+let io;
+
+const initSocket = (server) => {
+  io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
+  console.log("Socket.io initialized");
+
+  io.on("connection", (socket) => {
+    console.log(`New connection: ${socket.id}`);
+    registerHandlers(io, socket);
+  });
+
+  return io;
+};
+
+module.exports = { initSocket };
